6fbbefa2a27af5a8fba73b20c36a1faf1b4c2aa1
Attila Gergely <xgergelya1@stuba.sk>
2023-09-28 21:04:59 +0200

    feat: implement sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,18 +1,15 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/sysproc.c"
-#include "user/usys.s"
 
-int main(int argc,char *argv[])
-
-
-{
+int
+main(int argc, char *argv[]){
+  int ticks;
   if(argc<2){
-    frpintf(2,"Halo normalny\n");
-    exit(-1);
+    fprintf(2,"zle argumenty\n");
   }
-  sleep(atoi(argv[1]));
+  ticks=atoi(argv[1]);
+  fprintf(2,"Funguje to!\n");
+  sleep(ticks);
   exit(0);
 }
-
